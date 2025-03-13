64b6434bfb3fed76714a078224f15a1499c56ee2
XiangqiMeng <m332296992@gmail.com>
2025-03-13 10:59:57 -0400

    Event System: timer and pointer

--- Assets/Scripts/EventDemo.cs
+++ Assets/Scripts/EventDemo.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+ using UnityEngine.EventSystems;
+//using textMeshPro
 
 public class EventDemo : MonoBehaviour
 {
+    public RectTransform banana;
     // Start is called before the first frame update
-    void Start()
+    // public TMPro.TextMeshPro mPro;
+
+    public UnityEvent OnTimeHasFinish;
+    public float timerLength = 3;
+    public float t;
+
+    public void MouseJustEnteredImage()
     {
-       //initialize
+        Debug.Log("Hello?");
+        banana.localScale = Vector3.one * 1.2f;
     }
 
+    public void MouseJustExitImage()
+    {
+        Debug.Log("Bye!");
+        banana.localScale = Vector3.one;
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        t += Time.deltaTime;
+        if (t > timerLength)
+        { 
+            OnTimeHasFinish.Invoke();
+            t = timerLength;
+        }
+       
     }
 }
