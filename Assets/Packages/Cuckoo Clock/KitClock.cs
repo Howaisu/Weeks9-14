81eae4861a20793cb305e859b586d02e114daf8a
XiangqiMeng <m332296992@gmail.com>
2025-03-20 10:10:07 -0400

    Make a clock

---
+++ Assets/Packages/Cuckoo Clock/KitClock.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KitClock : MonoBehaviour
+{
+    public Transform hourHand;
+    public Transform minuteHand;
+    public float timeAnHourTakes = 5;
+    // public bool brake;
+
+    Coroutine clockRunning;
+    public IEnumerator doOneHour;
+
+    public float t;
+    public int hour = 0;
+
+    public UnityEvent OnTheHour;
+
+    void Start()
+
+    {
+      //  brake = true;
+       clockRunning = StartCoroutine(MoveTheClock());
+
+
+    }
+
+    private IEnumerator MoveTheClock()
+    {
+
+      // while(brake == true)
+      while(true)
+        {
+            doOneHour = MoveTheClockHandOneHour();
+            yield return StartCoroutine(doOneHour);
+        }
+ 
+        
+    }
+
+    private IEnumerator MoveTheClockHandOneHour()
+    {
+        t = 0;
+        while (t < timeAnHourTakes)
+        {
+            t += Time.deltaTime;
+
+            minuteHand.Rotate(0, 0, -((360) / timeAnHourTakes) * Time.deltaTime);
+            hourHand.Rotate(0, 0, -((30) / timeAnHourTakes) * Time.deltaTime);
+
+            yield return null ;
+
+        }
+        hour++;
+        if (hour == 13)
+        {
+            hour = 1;
+        }
+        OnTheHour.Invoke();
+    
+    
+    }
+
+    public void StopTheClock()
+    {
+        //    brake = false;
+        if (clockRunning != null)
+        {
+            StopCoroutine(clockRunning);
+        }
+
+        if (doOneHour != null)
+        {
+            StopCoroutine(doOneHour);
+        }
+
+    }
+}
--- Assets/Scripts/CodingGym.cs
+++ Assets/Scripts/CodingGym.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class CodingGym : MonoBehaviour
 {
+    public bool switcher;
+    public AnimationCurve brightness;
+    public float t;
+    public Vector2 startLocation;
+    public Renderer theLight;
+
     // Start is called before the first frame update
     void Start()
     {
+        startLocation = transform.position; // Save the initial position
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (switcher) // Continuously update alpha only if switcher is true
+        {
+            t += Time.deltaTime; // Increment t by the time passed since last frame
+            UpdateAlpha(); // Update the alpha value based on the animation curve
+        }
+    }
+
+    void UpdateAlpha()
+    {
+        float alpha = brightness.Evaluate(t); // Evaluate the curve at time t
+        Debug.Log("t ="+t+"brightness" + brightness.Evaluate(t));
+        Color newColor = theLight.material.color;
+        newColor.a = alpha; // Set the alpha to the evaluated value
+        GetComponent<Renderer>().material.color = newColor; // Apply the new color with updated alpha
+    }
+
+    public void TurnOn()
+    {
+        switcher = true;
+        Debug.Log("It is " + switcher);
+        // t is incremented in Update to smoothly transition based on time
+    }
+
+    public void TurnOff()
+    {
+        switcher = false;
+        Debug.Log("It is " + switcher);
+        t = 0f; // Reset time
+        UpdateAlpha(); // Immediately apply the initial alpha state
     }
 }
