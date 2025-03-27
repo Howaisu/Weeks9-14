41c84ea7474f9c5f40e21622d7890d7239e507d7
XiangqiMeng <m332296992@gmail.com>
2025-03-27 10:54:13 -0400

    sound_foot steps1, adding

--- Assets/Scripts/Week11/PlayerController.cs
+++ Assets/Scripts/Week11/PlayerController.cs
@@ -8,14 +8,20 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     public float speed = 2;
     public bool canRun = true;
- 
+    public AudioClip[] footsteps; // 存储所有footstep音频的数组
+    private AudioSource audioSource; // 用于播放音频的组件
+   
 
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         animator = sr.GetComponent<Animator>();
-       
+        
+        //
+        audioSource = GetComponent<AudioSource>(); // 获取当前GameObject的AudioSource组件
+        
+
     }
 
     // Update is called once per frame
@@ -51,6 +57,9 @@ public class PlayerController : MonoBehaviour
     public void FootSteps()
     {
         Debug.Log("step on the groud.");
-    
+        int R = Random.Range(0, 9); // 随机选择一个索引
+        audioSource.clip = footsteps[R]; // 设置AudioSource要播放的clip
+        audioSource.Play(); // 播放音频
+
     }
 }
